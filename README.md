233942ba69a774fbffcd1f7bb3ae105aa52c8dc9
Nick <n.reeves429@csu.fullerton.edu>
2023-12-09 00:30:15 +0000

    Final Push

---
+++ README.md
@@ -0,0 +1,10 @@
+This website is designed to be an easy resource for those looking into working out. The different pages and their functions are described below:
+
+index.html-  the hub, to show the different pages with links to each one
+meal.html-   a page providing descriptions and links to simple and healthy recipes for all means
+supplements.html -   a page providing info on workout supplements
+exercises.html- a page that allows users to find exercises based on their target muscle group
+tips.html - a page that allows users to generate inpirational images at the click of a button
+workouttips.html - allows the user to scroll through a list of helpful workout tips
+resources.html - a list of external links for other helpful resources
+about.html - a page showing information about the page and its devs (including me!)
---
+++ index.js
@@ -0,0 +1,78 @@
+import { Accordion } from 'flowbite';
+/*
+* accordionEl: HTML element (required)
+* accordionItems: array of accordion item objects (required)
+* options (optional)
+* instanceOptions (optional)
+*/
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Toggle Navbar
+  var menuButton = document.querySelector('[data-collapse-toggle="navbar-sticky"]');
+  var menu = document.getElementById("navbar-sticky");
+  menuButton.addEventListener("click", function() {
+    menu.classList.toggle("hidden");
+  });
+
+
+  /* accordion */
+
+  const accordionElement = document.getElementById('accordion-example');
+
+  // create an array of objects with the id, trigger element (eg. button), and the content element
+  const accordionItems = [
+    {
+      id: 'accordion-example-heading-1',
+      triggerEl: document.querySelector('#accordion-example-heading-1'),
+      targetEl: document.querySelector('#accordion-example-body-1'),
+      active: true
+    },
+    {
+      id: 'accordion-example-heading-2',
+      triggerEl: document.querySelector('#accordion-example-heading-2'),
+      targetEl: document.querySelector('#accordion-example-body-2'),
+      active: false
+    },
+    {
+      id: 'accordion-example-heading-3',
+      triggerEl: document.querySelector('#accordion-example-heading-3'),
+      targetEl: document.querySelector('#accordion-example-body-3'),
+      active: false
+    }
+  ];
+
+  // options with default values
+  const options = {
+    alwaysOpen: true,
+    activeClasses: 'bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-white',
+    inactiveClasses: 'text-gray-500 dark:text-gray-400',
+    onOpen: (item) => {
+      console.log('accordion item has been shown');
+      console.log(item);
+    },
+    onClose: (item) => {
+      console.log('accordion item has been hidden');
+      console.log(item);
+    },
+    onToggle: (item) => {
+      console.log('accordion item has been toggled');
+      console.log(item);
+    },
+  };
+
+  // instance options object
+  const instanceOptions = {
+    id: 'accordion-example',
+    override: true
+  };
+  // Initialize Accordion
+  const accordion = new Accordion(accordionElement, accordionItems, options, instanceOptions);
+
+  // open accordion item based on id
+  accordion.open('accordion-example-heading-2');
+
+  // close accordion item based on id
+  accordion.close('accordion-example-heading-2');
+
+  // toggle visibility of item based on id
+  accordion.toggle('accordion-example-heading-3');
